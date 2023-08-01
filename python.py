eb37a352f0d88a67b4f2f17a89be8ecae0ab82e1
Basnet4567 <it212202@tridenta.ac.jp>
2023-08-01 22:57:01 +0900

    python.py

--- python.py
+++ python.py
@@ -3,3 +3,18 @@ print(userInput)
 
 print(123)
 print("test test test")
+<<<<<<< Updated upstream
+=======
+for i in range(1, 100):
+    if i % 2 == 0:
+        print(f"{i} is even")
+    else:
+        print(f"{i} is odd")
+
+while True:
+    user = int(input('1 to 10'))
+    if user % 2 == 1:
+        print(user*100)
+    else:
+        print(user*10)
+>>>>>>> Stashed changes
